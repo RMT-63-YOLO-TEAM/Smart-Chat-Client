2ce4a950321d5d11c94818726178a80e86da506d
Muh. Fayiz Syamsuddin <muhfayiz14@gmail.com>
2025-07-30 11:40:02 +0800

    feat: initialize client application with React and Vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//* Jenis nya -> Event based
+//* -> Emit -> Mengirimkan request (client -> server)
+//* -> On -> Menerima request
+
+function App() {
+  return (
+    <div>
+      <h1>Chat App</h1>
+    </div>
+  );
+}
+
+export default App;
